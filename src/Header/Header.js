84e214c4e86ae040bdbafd65ad9df8e599dd61be
fazlay26 <rabboi007@gmail.com>
2022-03-25 21:20:14 +0600

    part1

---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-title'>
+            <h2>Watch Giveaways</h2>
+            <h4>Choose 4 watch</h4>
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Header/Shop/Shop.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+
+            <div className='products-container'>
+                <h2>this is prodcuts compo</h2>
+
+            </div>
+            <div className='selected-products-container'>
+                <h2>this is selected products compo</h2>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Shop;
